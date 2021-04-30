8fa7234d7d3d66212a5a888d7c4545f65489ac79
Aldiansyah <aldiansyahunip@gmail.com>
2021-04-30 14:29:16 +0700

    initial commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,118 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>--Register--</title>
+
+    <link rel="stylesheet" href="{{ asset('css/backend.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('select2/dist/css/select2.min.css')}}">
+
+    <!-- Script -->
+    <script src="{{asset('select2/jquery-3.4.1.js')}}" type="text/javascript"></script>
+    <script src="{{asset('select2/dist/js/select2.min.js')}}" type="text/javascript"></script>
+</head>
+<body>
+    <div class="boxlogin">
+        <div class="boxlogin1">
+            <div class="logoregis">
+                <h1 class="text-regis">Halaman <b>Dafar</b></h1>
+                <p class="text-parap">Silahkan lengkapi data diri dibawah ini</p>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                            <input id="name" type="text" class="form-nameregis @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Nama Lengkap">
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <select id="regions" name="region_id" class="form-regions @error('name') is-invalid @enderror" style="width: 300px;">
+                                <option value="">--Name Region--</option>
+                            </select>
+                            @error('region_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <input id="email" type="email" class="form-emailregis @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <input id="password" type="password" class="form-passregis @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <input type="number" class="form-phoneregis @error('password') is-invalid @enderror" name="phone" required autocomplete="phone" placeholder="Nomor WhatsApp Aktif">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    <button type="submit" class="button1">
+                        {{ __('Register') }}
+                    </button>
+
+                    <p class="linktextakun1">Sudah Punya Akun?</p>
+                    @if (Route::has('login'))
+                            <a class="linkregis1" href="{{ route('login') }}">"Masuk"</a>
+                        @endif
+                </form>
+
+                <div class="box1">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+
+        // CSRF Token
+        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+        $(document).ready(function(){
+
+          $( "#regions" ).select2({
+            ajax: {
+              url: "{{route('region.getRegion')}}",
+              type: "get",
+              dataType: 'json',
+              delay: 250,
+              data: function (params) {
+                return {
+                  _token: CSRF_TOKEN,
+                  search: params.term // search term
+                };
+              },
+              processResults: function (response) {
+                return {
+                  results: response
+                };
+              },
+              cache: true
+            }
+
+          });
+
+        });
+        </script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -74,4 +188,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
